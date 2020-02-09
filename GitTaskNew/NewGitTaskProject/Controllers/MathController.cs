acc994658b60b29b55b6c8a6dd07a244df27859e
Diaa Abd <diaa@applez.tech>
2020-02-09 11:54:45 +0200

    MathOperation Created

---
+++ GitTaskNew/NewGitTaskProject/Controllers/MathController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewGitTaskProject.Models;
+
+namespace NewGitTaskProject.Controllers
+{
+    public class MathController : Controller
+    {
+        // GET: Math
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(double NUM1, double NUM2)
+        {
+            Models.SimpleMath A = new SimpleMath();
+            A.FirstNumber = NUM1;
+            A.SecondNumber = NUM2;
+
+            ViewBag.T = A.GetResult();
+            return View();
+        }
+    }
+}
---
+++ GitTaskNew/NewGitTaskProject/Models/SimpleMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewGitTaskProject.Models
+{
+    public class SimpleMath
+    {
+        public double? FirstNumber { get; set; }
+        public double? SecondNumber { get; set; }
+
+        public string GetResult()
+        {
+            string result = string.Empty;
+            if (FirstNumber.HasValue && SecondNumber.HasValue)
+            {
+                result = (FirstNumber + SecondNumber).ToString();
+            }
+
+            return result;
+        }
+    }
+}
