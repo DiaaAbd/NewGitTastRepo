ed331a6a2335290a6e1002fd23f34e377e7861bd
Anas <Anas@applez.tech>
2020-02-09 13:08:30 +0200

    a

--- GitTaskNew/NewGitTaskProject/Models/SimpleMath.cs
+++ GitTaskNew/NewGitTaskProject/Models/SimpleMath.cs
@@ -15,11 +15,9 @@ namespace NewGitTaskProject.Models
             string result = string.Empty;
             if (FirstNumber.HasValue && SecondNumber.HasValue)
             {
-<<<<<<< HEAD
+
                 result = (FirstNumber / SecondNumber).ToString();
-=======
-                result = (FirstNumber * SecondNumber).ToString();
->>>>>>> 8f494e9576f55d38702d3a542f00be84ad59d047
+
             }
 
             return result;
